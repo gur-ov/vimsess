3da2aecbbe726c4897e9467a552dd81d6e066a7d
gur-ov <gurov.mobile@gmail.com>
2021-09-15 16:01:06 +0200

    Первое врнесение данных в репозиторий, создание основных модулей.

---
+++ control.py
@@ -0,0 +1,12 @@
+# Проверка подкл.чения библиотеки sys в списке пременных программы
+
+import var_list
+
+# print var_list.p_pwd
+
+print (var_list.pwd_print)
+print (var_list.tell)
+print (var_list.ls_print)
+print(var_list.checklist)
+
+print('fdfd')
